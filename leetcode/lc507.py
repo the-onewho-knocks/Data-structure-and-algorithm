76ae7e8be957fc0b63cfb77b41c388a7fc389ca5
the-onewho-knocks <borsehardik@gmail.com>
2026-01-22 18:25:35 +0530

    jifjweogi

--- leetcode/lc507.py
+++ leetcode/lc507.py
@@ -1,11 +1,28 @@
+from math import sqrt
+
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
-        sum = 0
-        for i in range(1 , num):
-            if num % i == 0:
-                sum += i
-        return sum == num
-
+        if num<=1:
+            return False
+        sum=1
+        for i in range(2,int(sqrt(num))+1):
+            if num%i==0:
+                sum+=i
+                print(sum)
+                if i!=num:
+                    sum+=num//i
+                    print(sum)
+        return sum==num
+    
 if __name__ == "__main__":
     sol = Solution()                
     print(sol.checkPerfectNumber(28))
+
+
+
+# Example 1:
+
+# Input: num = 28
+# Output: true
+# Explanation: 28 = 1 + 2 + 4 + 7 + 14
+# 1, 2, 4, 7, and 14 are all divisors of 28.
