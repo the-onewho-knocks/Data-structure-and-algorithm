212a74f80b32129eec3242e0771540afd719074a
the-onewho-knocks <borsehardik@gmail.com>
2026-01-22 19:00:32 +0530

    ehtnfg

---
+++ leetcode/lc2427.py
@@ -0,0 +1,64 @@
+from math import sqrt
+# Example 1:
+
+# Input: a = 12, b = 6
+# Output: 4
+# Explanation: The common factors of 12 and 6 are 1, 2, 3, 6.
+# Example 2:
+
+# Input: a = 25, b = 30
+# Output: 2
+# Explanation: The common factors of 25 and 30 are 1, 5.
+
+# a = 12
+# b = 6
+
+# a_list = []
+# b_list = []
+# result = []
+
+# for i in range(1 ,int((a))):
+#     if a % i == 0:
+#         a_list.append(i)
+# print(a_list)
+
+# for i in range(1 ,int((b))+1):
+#     if int(b % i) == 0:
+#         b_list.append(i)
+# print(b_list)
+
+# for i in a_list:
+#     for j in b_list:
+#         if i == j:
+#             result.append(i)
+# print(len(result))
+
+from math import sqrt
+
+class Solution:
+    def commonFactors(self, a: int, b: int) -> int:
+        a_list = []
+        b_list = []
+        result= []
+        for i in range(1 , int((a))+1):
+            if a % i == 0:
+                a_list.append(i)
+                print(a_list)
+
+        for i in range(1 , int((b))+1):
+            if b % i == 0:
+                b_list.append(i)
+                print(b_list)
+
+        for i in a_list:
+            for j in b_list:
+                if i == j:
+                    result.append(i)
+        return len(result)
+    
+if __name__ == "__main__":
+    sol = Solution()                
+    print(sol.commonFactors(885,885))
+
+
+    
