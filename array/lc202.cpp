0e502929d66ce6f38ea5211ce29a7628193986a6
the-onewho-knocks <borsehardik@gmail.com>
2026-05-20 23:09:47 +0530

    feat: Implement Happy Number solution (LC 202)
    Introduces the solution for LeetCode 202, "Happy Number", utilizing Floyd's Cycle-Finding algorithm. Adds a Solution class containing `isHappy` and a helper for digit sum processing. Previous unrelated code has been replaced.

--- array/lc202.cpp
+++ array/lc202.cpp
@@ -1,28 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+    int sumOftheDigitsOfnums(int n)
+    {
+        int sum = 0;
+        n = n % 10;
+        while (n > 0)
+        {
+            int digit = n * n;
+            sum = sum + digit;
+            n = n / 10;
+        }
+        return sum;
+    }
+
+bool isHappy(int n) {
+
+        int slow = n;
+        int fast = n;
+
+        do{
+            slow = sumOftheDigitsOfnums(slow);
+
+            fast = sumOftheDigitsOfnums(
+                    sumOftheDigitsOfnums(fast));
+
+        }while(slow != fast);
+
+        return slow == 1;
+    }
+};
+
+
+int main(){
+    int n = 2;
+    Solution sol;
+
+    sol.isHappy(n);
+}
+
 // //fast and slow
 // Input: [1,2,3,4,5]
 // Output: 3
 
-int main(){
+// int main(){
 
-    string v = "PYTHON";
-    // vector<int> v = {1 , 2 , 3 , 4 ,5};
-    int slow = 0;
-    int fast = 0;
+//     string v = "PYTHON";
+//     // vector<int> v = {1 , 2 , 3 , 4 ,5};
+//     int slow = 0;
+//     int fast = 0;
 
-    while(fast < v.size() && fast+1 < v.size()){
-        slow = slow+1;
-        fast = fast + 2;
+//     while(fast < v.size() && fast+1 < v.size()){
+//         slow = slow+1;
+//         fast = fast + 2;
 
-       // cout<<v[slow]<<endl;
-    }
+//        // cout<<v[slow]<<endl;
+//     }
 
-    cout<<v[slow]<<endl;
-}
+//     cout<<v[slow]<<endl;
+// }
 
-// int main() 
+// int main()
 // {
 //     int n = 19;
 //     int ans;
