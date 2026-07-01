038783461b64f57b663e9e07f10edb0724eff035
the-onewho-knocks <borsehardik@gmail.com>
2026-07-01 19:08:06 +0530

    refactor(sliding-window): Rework 3254.cpp logic; new examples
    Reworked `3254.cpp` sliding window to a standard two-pointer approach. This correctly identifies strictly increasing sequences within `k`-sized windows, replacing the prior logic. Also, new sliding window examples added to `examples.cpp`.

--- sliding-window/3254.cpp
+++ sliding-window/3254.cpp
@@ -1,69 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T>
+bool is_sorted(const std::vector<T> &vec)
+{
+    return std::is_sorted(vec.begin(), vec.end());
+}
+
+// // 2. Check if a vector has all unique elements (No duplicates)
+// // Time Complexity: O(N) | Space Complexity: O(N)
+template <typename T>
+bool is_unique(const std::vector<T> &vec)
+{
+    std::unordered_set<T> unique_set(vec.begin(), vec.end());
+    return vec.size() == unique_set.size();
+}
+
+template <typename T>
+T find_max_element(const std::vector<T> &vec)
+{
+    // Safety check: An empty vector has no maximum element
+    if (vec.empty())
+    {
+        throw std::invalid_argument("Cannot find maximum of an empty vector.");
+    }
+
+    // std::max_element returns an iterator, so we use '*' to get the value
+    return *std::max_element(vec.begin(), vec.end());
+}
+
 int main()
 {
     vector<int> nums = {1, 2, 3, 4, 3, 2, 5};
     int k = 3;
     vector<int> ans;
-    int i = 0;
-    int j = 0;
-    int maxi = INT_MIN;
-    vector<int> v;
 
-    while (i < nums.size())
-    {
+    int left = 0;
 
-        while (j < k)
-        {
-            if (nums[i] < nums[j] && nums[i] != nums[j] && nums[i] < nums[j + 1] && nums[i] != nums[j + 1] && nums[j] < nums[j + 1] && nums[j] != nums[j + 1])
-            {
-                maxi = max({nums[i], nums[j], nums[j + 1]});
-            }
-            else
-            {
-                ans.push_back(-1);
-            }
-            ans.push_back(maxi);
-            ++j;
+    for(int right = 0 ; right < nums.size() ; ++right){
+        int sum = right - left + 1;
+
+        if(sum == k){
+
+                if(nums[left] < nums[left+1]  && nums[left+1] < nums[right]){
+                    ans.push_back(nums[right]);
+                }
+                else{
+                    ans.push_back(-1);
+                }
+
+            ++left;
         }
-        j = j - k + 1;
-        ++i;
     }
 
-    for (int x : ans)
-    {
-        cout << x << endl;
+
+    for(int x : ans){
+        cout<<x<<" ";
     }
+    cout<<endl;
 }
 
-// template <typename T>
-// bool is_sorted(const std::vector<T> &vec)
-// {
-//     return std::is_sorted(vec.begin(), vec.end());
-// }
 
-// // 2. Check if a vector has all unique elements (No duplicates)
-// // Time Complexity: O(N) | Space Complexity: O(N)
-// template <typename T>
-// bool is_unique(const std::vector<T> &vec)
-// {
-//     std::unordered_set<T> unique_set(vec.begin(), vec.end());
-//     return vec.size() == unique_set.size();
-// }
-
-// template <typename T>
-// T find_max_element(const std::vector<T> &vec)
-// {
-//     // Safety check: An empty vector has no maximum element
-//     if (vec.empty())
-//     {
-//         throw std::invalid_argument("Cannot find maximum of an empty vector.");
-//     }
-
-//     // std::max_element returns an iterator, so we use '*' to get the value
-//     return *std::max_element(vec.begin(), vec.end());
-// }
 
 // int main()
 // {
--- sliding-window/examples.cpp
+++ sliding-window/examples.cpp
@@ -3,5 +3,36 @@ using namespace std;
 
 int main()
 {
+    // vector<int> v = {1 , 2 , 3, 4 , 5,6};
+    // int k = 3;
+    // int left = 0;
+    // for(int right = 0 ; right < v.size() ; ++right ){
+    //     int size = right - left + 1;
 
+    //     if(size == k){
+    //         for(int i = left ; i <=right ; ++i){
+    //             cout<<v[i]<< " ";
+    //         }
+    //         cout<<endl;
+
+    //         ++left;
+    //     }
+    // }
+
+    string s = "abcdef";
+    int k = 3;
+
+    int left = 0;
+    for(int right = 0 ; right < s.length() ; ++right){
+        int size = right - left + 1;
+
+        if(size == k){
+
+            for(int i = left; i <= right ; ++i){
+                cout<<s[i]<<" ";
+            }
+            cout<<endl;
+            ++left;
+        }
+    }
 }
