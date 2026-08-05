ac315b77ce0daec17622580e7e5ab42a338bd8be
the-onewho-knocks <borsehardik@gmail.com>
2026-08-05 23:13:52 +0530

    refactor: Optimize sliding window logic for character replacement problem

--- neetcode150/424.cpp
+++ neetcode150/424.cpp
@@ -1,34 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 int main(){
     string s = "ABAB";
-    string a = s;
     int k = 2;
-    int maxi = 1;
-     int count = 1;
-    int left = 0;
 
-    for(int right = 1 ; right < s.size() ; ++right){
+    vector<int>freq(26 , 0);
+
+    int left = 0;
+    int maxfreq = 0;
+    int ans = 0;
+
+    for(int right = 0 ; right < s.size() ; right++){
+
+        freq[s[right] - 'A']++;
+
+        maxfreq = max(maxfreq , freq[s[right] - 'A']);
+
+        while((right - left + 1) - maxfreq > k){
+            freq[s[left] - 'A']--;
+            left++;
+        }
+
+        ans = max(ans , right - left + 1);
+    }
+
+    cout<<ans<<endl;
+}
+// int main(){
+//     string s = "ABAB";
+//     string a = s;
+//     int k = 2;
+//     int maxi = 1;
+//      int count = 1;
+//     int left = 0;
+
+//     for(int right = 1 ; right < s.size() ; ++right){
 
         
-    }
+//     }
 
-        cout<<a<<endl;
+//         cout<<a<<endl;
 
-    for (int i = 1; i < a.size(); i++) {
-        if (a[i] == a[i - 1]) {
-            count++;
-        } else {
-            maxi = max(maxi, count);
-            count = 1;
-        }
-    }
-        maxi = max(maxi, count);
+//     for (int i = 1; i < a.size(); i++) {
+//         if (a[i] == a[i - 1]) {
+//             count++;
+//         } else {
+//             maxi = max(maxi, count);
+//             count = 1;
+//         }
+//     }
+//         maxi = max(maxi, count);
 
 
-    cout<<maxi;
-}
+//     cout<<maxi;
+// }
 
   //  sort(a.rbegin() , a.rend());
   //  cout<<a<<endl;
